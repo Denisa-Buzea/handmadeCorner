7f1a9674ae42d41d1e36a1281f5041a517d37874
BogdanCC <bogdan.e.corcoveanu@gmail.com>
2018-05-29 22:12:19 +0300

    First stage update

--- includes/head.php
+++ includes/head.php
@@ -4,15 +4,13 @@
   <title>Coltisorul Handmade</title>
   <!-- folosim bootstrap minified pt. ca e mai rapid-->
   <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/main.css">
   <!-- pt. responsive bootstrap -->
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"><!-- asigura ca merge pe mai multe device-uri-->
   <!-- includem bJquery si bootstrap js -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
-
   <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
-  
+  <link rel="stylesheet" href="css/main.css">
 
 
 </head>
--- index.php
+++ index.php
@@ -7,29 +7,41 @@ include 'includes/navigation.php';
 <!-- I updated -->
 <!-- Acum suntem pe parallax -->
 <body id="main_page">
-<div>
   <div>
-  <?php
-  if(has_permission('admin')): ?>
-  <a href="/handmadeCorner/admin/administrator.php" class="btn btn-warning" role="button">Admin</a>
-  <?php endif
-  ?>
-  <a href="shop.php" class="btn btn-info" role="button">Featured Products</a>
-  <?php
-      if(!is_logged_in()) {
-  ?>
-    <a href="admin/login.php" class="btn btn-warning" role="button">Log in</a>
-  <a href="register.php" class="btn btn-success" role="button">Sign Up</a>
-<?php } ?>
-  <p><?php
-echo 'Current PHP version: ' . phpversion();
-?></p>
-</div>
-</div>
+    <div>
+    <?php
+      if(has_permission('admin')): ?>
+      <a href="/handmadeCorner/admin/administrator.php" class="btn btn-warning" role="button">Admin</a>
+      <?php endif  ?>
+      <a href="shop.php" class="btn btn-info" role="button">Featured Products</a>
+      <?php if(!is_logged_in()) {  ?>
+        <a href="admin/login.php" class="btn btn-warning" role="button">Log in</a>
+        <a href="register.php" class="btn btn-success" role="button">Sign Up</a>
+      <?php } ?>
+      <p><?php
+    echo 'Current PHP version: ' . phpversion(); ?>
+      </p>
+    </div>
+  </div>
+  <div class="parallax-bg">
+    <div class="container12">
+      <ul id="scene">
+        <li class="layer" data-depth="0.2">My first Layer!</li>
+        <li class="layer" data-depth="0.6">My second Layer!</li>
+        <li class="layer" data-depth="0.4">My first Layer!</li>
+        <li class="layer" data-depth="0.8">My second Layer!</li>
+      </div>
+    </ul>
+  </div>
+  <script src="js/parallax.min.js"></script>
+  <script>
+    var scene = document.getElementById('scene');
+    var parallax = new Parallax(scene, {
+      relativeInput : true
+    });
+    parallax.friction(0.2, 0.2);
+  </script>
 </body>
-
 <?php
-
   include 'includes/footer.php';
-
  ?>
